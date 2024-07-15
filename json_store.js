8083e639e5a967d932009e8a948e017ee2b0e1ed
zy-zhangyu <dayuya_zy@163.com>
2024-07-15 10:29:39 +0800

    1

--- json_store.js
+++ json_store.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 
